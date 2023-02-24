2389e1becf9c1a610c012fedd4708485e88f08e7
arpanmerkle <arpan.garg@merkle.com>
2023-02-24 09:18:52 +0100

    Adding initial commit for animation in column block

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,4 +1,19 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
+
+  window.addEventListener('scroll', function() {
+    var element = document.querySelectorAll('div.facts > div > div > p ');
+    element.forEach((ele, i) => {
+      var position = ele.getBoundingClientRect();
+      if (position.top >= 0 && position.bottom <= window.innerHeight) {
+        ele.style.setProperty("--num",100);
+        console.log(ele.innerHTML);
+      }
+      if (position.top < window.innerHeight && position.bottom >= 0) {
+        ele.style.setProperty("--num",100);
+        console.log(ele.innerHTML);
+      }
+    });
+  });
 }
