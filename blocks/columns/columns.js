de008d8fda1362180ccbfb88f2ccf54ebb409308
arpanmerkle <arpan.garg@merkle.com>
2023-02-24 09:54:29 +0100

    Adding few changes to improve readability

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,18 +2,20 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
-  window.addEventListener('scroll', function() {
-    var element = document.querySelectorAll('div.facts > div > div > p ');
+  window.addEventListener('scroll', () => {
+    const element = document.querySelectorAll('div.facts > div > div > p ');
     element.forEach((ele, i) => {
-      var position = ele.getBoundingClientRect();
-      if (position.top >= 0 && position.bottom <= window.innerHeight) {
-        ele.style.setProperty("--num",100);
-        console.log(ele.innerHTML);
-      }
-      if (position.top < window.innerHeight && position.bottom >= 0) {
-        ele.style.setProperty("--num",100);
-        console.log(ele.innerHTML);
-      }
+      setProp(ele);
     });
   });
+}
+
+function setProp(ele) {
+  const position = ele.getBoundingClientRect();
+  if (position.top >= 0 && position.bottom <= window.innerHeight) {
+    ele.style.setProperty('--num', 100);
+  }
+  if (position.top < window.innerHeight && position.bottom >= 0) {
+    ele.style.setProperty('--num', 100);
+  }
 }
